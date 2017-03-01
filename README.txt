aa3339ed021411c444b4ef27559f49b766529fa8
ragog <grago@testobject.com>
2017-03-01 16:36:53 +0100

    Init.

---
+++ README.txt
@@ -0,0 +1,64 @@
+## Java-TestNG-Appium-Android-REST
+
+>This code is presented as an example only, since your tests and testing environments may require specialized scripting. This information should be taken only as an
+>illustration of how one would set up tests with Sauce Labs, and any modifications will not be supported. For questions regarding Sauce Labs integration, please see 
+>our documentation at https://wiki.saucelabs.com/.
+
+### Environment Setup
+
+1. Global Dependencies
+    * Install Maven
+    	https://maven.apache.org/install.html
+    * Or Install Maven with Homebrew
+    	http://brew.sh/
+    ```
+    $ brew install maven
+    ```
+2. TestObject Credentials
+    * Add your TestObject API Key
+    ```
+    $ export TESTOBJECT_API_KEY=<your project's api key>
+    ```
+
+3. Project Dependencies
+    * Check that packages are available
+    ```
+    $ cd Java-TestNG-Appium-Android-REST
+    $ mvn test-compile
+    ```
+    * You may also want to run the command below to check for outdated dependencies. Please be sure to verify and review updates before editing your pom.xml file as they may not be compatible with your code.
+    ```
+    $ mvn versions:display-dependency-updates
+    ```
+4. Application
+    * Application can be downloaded by clicking [here](https://github.com/testobject/calculator-test-gradle/blob/master/Calculator_2.0.apk)
+    
+### Running Tests
+	* Note: pom.xml settings and testng.xml need to be updated if you wish to run on more than 2 devices at once.
+
+#####Execute TestNG Suite:
+	```
+	$ mvn -X clean test -U -Pappium-tests
+	```
+
+### Advice/Troubleshooting
+1. It may be useful to use a Java IDE such as IntelliJ or Eclipse to help troubleshoot potential issues. 
+
+### Resources
+##### [TestObject REST API Documentation](https://api.testobject.com/)
+
+##### [Sauce Labs Documentation](https://wiki.saucelabs.com/)
+
+##### [TestNG Documentation](http://testng.org/doc/documentation-main.html)
+
+##### [Java Documentation](https://docs.oracle.com/javase/7/docs/api/)
+
+##### [Stack Overflow](http://stackoverflow.com/)
+* A great resource to search for issues not explicitly covered by documentation.
+
+
+
+
+
+
+
---
+++ src/test/java/com/saucelabs/example/TestSetup.java
@@ -0,0 +1,71 @@
+package com.saucelabs.example;
+
+import com.saucelabs.example.util.ResultReporter;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by grago on 28/02/2017.
+ */
+public class TestSetup {
+
+    private AndroidDriver driver;
+    private ResultReporter reporter;
+
+    @BeforeMethod
+    @Parameters({ "device" })
+    public void setUp(String device) throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY"));
+        capabilities.setCapability("testobject_device", device);
+
+        driver = new AndroidDriver(new URL("http://appium.testobject.com/wd/hub"), capabilities);
+        reporter = new ResultReporter();
+
+    }
+
+    /* A simple addition, it expects the correct result to appear in the result field. */
+    @Test
+    public void twoPlusTwoOperation() throws MalformedURLException {
+
+        /* Get the elements. */
+        MobileElement buttonTwo = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/digit2")));
+        MobileElement buttonPlus = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/plus")));
+        MobileElement buttonEquals = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/equal")));
+        MobileElement resultField = (MobileElement)(driver.findElement(By.xpath("//android.widget.EditText[1]")));
+
+        /* Add two and two. */
+        buttonTwo.click();
+        buttonPlus.click();
+        buttonTwo.click();
+        buttonEquals.click();
+
+        /* Check if within given time the correct result appears in the designated field. */
+        (new WebDriverWait(driver, 30)).until(ExpectedConditions.textToBePresentInElement(resultField, "4"));
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        boolean success = result.isSuccess();
+        String sessionId = driver.getSessionId().toString();
+
+        reporter.saveTestStatus(sessionId, success);
+        driver.quit();
+    }
+
+}
